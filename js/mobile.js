176167bae62878aa8973b9af05f9ed91dd5cbce2
Hossam Mamdouh <hossammamdouh8010@gmail.com>
2020-06-06 03:23:35 +0200

    mobile auto play

---
+++ js/mobile.js
@@ -0,0 +1,19 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: 'NuIAYHVeFYs',
+      playerVars: { 'autoplay': 1, 'playsinline': 1 },
+      events: {
+        'onReady': onPlayerReady
+      }
+    });
+  }
+
+  function onPlayerReady(event) {
+     event.target.mute();
+    event.target.playVideo();
+  }
